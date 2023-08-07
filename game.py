20251b5c93889392161a57197d1a38a64e3730e1
natejenn9 <91986684+natejenn9@users.noreply.github.com>
2023-08-07 14:25:41 -0400

    game.py
    added infinite bg

--- game.py
+++ game.py
@@ -1,13 +1,52 @@
 import pygame
+import math
 pygame.init()
+
+
 
 #constants
 WIDTH = 700
 HIGHT = 600
+timer = pygame.time.Clock()
+fps = 60
 
 #screen name icon caption ect
 screen = pygame.display.set_mode([WIDTH, HIGHT])
 pygame.display.set_caption("my game")
+
+#backgound stuff
+bg = pygame.image.load('bg.png')
+class Background():
+	def __init__(self, win):
+		self.win = win
+
+		self.image = pygame.image.load('bg.png')
+		self.image = pygame.transform.scale(self.image, (WIDTH, HIGHT))
+		self.rect = self.image.get_rect()
+
+		self.reset()
+		self.move = True
+
+	def update(self, speed):
+		if self.move:
+			self.y1 += speed
+			self.y2 += speed
+
+			if self.y1 >= HIGHT:
+				self.y1 = -HIGHT
+			if self.y2 >= HIGHT:
+				self.y2 = -HIGHT
+
+		self.win.blit(self.image, (self.x,self.y1))
+		self.win.blit(self.image, (self.x,self.y2))
+
+	def reset(self):
+		self.x = 0
+		self.y1 = 0
+		self.y2 = -HIGHT
+
+bg = Background(screen)
+
 
 #donut player
 donut_img = pygame.image.load('donut.png')
@@ -23,20 +62,22 @@ def donut(x,y):
 #game loop
 running = True
 while running:
-    
-    screen.fill((255, 255, 255))
+    timer.tick(fps)
 
+    bg.update(3)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                change_x = -2.1
+                change_x = -4
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                change_x = 2.1
+                change_x = 4
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_a  or event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 change_x = 0
+    
 
     donut_x += change_x
     donut(donut_x, donut_y)
